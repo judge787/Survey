9d43261d8fbdd3920b39b23450973088776c5dbd
will0082 <william.childs@rogers.com>
2023-05-09 15:51:16 -0400

    Got .xls files working

--- templates/myscript.js
+++ templates/myscript.js
@@ -1,25 +1,45 @@
-// Function to handle file input change event
-function handleFile(e) {
-    var file = e.target.files[0];
-    readXlsxFile(file).then(function (rows) {
-      autofillForm(rows);
-    });
-  }
-  
-  // Function to autofill form fields
-  function autofillForm(rows) {
-    // add a for loop to run through rows
+function convertToJSON() {
+  var fileInput = document.getElementById("xlsFile");
+  var file = fileInput.files[0];
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    var data = new Uint8Array(e.target.result);
+    var workbook = XLSX.read(data, { type: "array" });
+    var worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    var jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+    displayOutput(jsonData);
+  };
+  reader.readAsArrayBuffer(file);
+}
 
-    var headerRow = rows[0]; // Assuming the first row contains the column headers
-    var dataRow = rows[1]; // Assuming the second row contains the data
-    var nameIndex = headerRow.indexOf("Name");
-    var emailIndex = headerRow.indexOf("Email");
-    var ageIndex = headerRow.indexOf("Age");
-    document.getElementById("name").value = dataRow[nameIndex];
-    document.getElementById("email").value = dataRow[emailIndex];
-    document.getElementById("age").value = dataRow[ageIndex];
-  }
+function displayOutput(jsonData) {
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = JSON.stringify(jsonData, null, 2);
+}
+
+
+
+
+// // Function to handle file input change event
+// function handleFile(e) {
+//     var file = e.target.files[0];
+//     readXlsxFile(file).then(function (rows) {
+//       autofillForm(rows);
+//     });
+//   }
   
-  // Bind the handleFile function to file input change event
-  document.getElementById("excelFile").addEventListener("change", handleFile, false);
+//   // Function to autofill form fields
+//   function autofillForm(rows) {
+//     var headerRow = rows[0]; // Assuming the first row contains the column headers
+//     var dataRow = rows[1]; // Assuming the second row contains the data
+//     var nameIndex = headerRow.indexOf("Name");
+//     var emailIndex = headerRow.indexOf("Email");
+//     var ageIndex = headerRow.indexOf("Age");
+//     document.getElementById("name").value = dataRow[nameIndex];
+//     document.getElementById("email").value = dataRow[emailIndex];
+//     document.getElementById("age").value = dataRow[ageIndex];
+//   }
+  
+//   // Bind the handleFile function to file input change event
+//   document.getElementById("excelFile").addEventListener("change", handleFile, false);
   
