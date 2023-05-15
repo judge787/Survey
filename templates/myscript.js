425977d16065df5c04fb8fbca7c197e98d2f76f6
judge787 <jashanjudge87@gmail.com>
2023-05-15 13:34:04 -0400

    q13 input text working, now im working on outputting the right values

--- templates/myscript.js
+++ templates/myscript.js
@@ -1,76 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  function createCheckboxAndInputPair(index, labelText, checkboxValue) {
-    const letter = String.fromCharCode(96 + index);
-    const nextLetter = String.fromCharCode(97 + index);
-
+  function createCheckboxAndInputPair(checkboxId, containerId, labelText, checkboxValue) {
     return `
       <div class="adjust-size">
-        <label for="q13${letter}${index}">${labelText}</label>
-        <input type="checkbox" id="q13${letter}${index}" name="q13${letter}${index}" value="${checkboxValue}">
+        <label for="${checkboxId}">${labelText}</label>
+        <input type="checkbox" id="${checkboxId}" name="${checkboxId}" value="${checkboxValue}">
       </div>
-      <div class="adjust-size" id="q13${nextLetter}${index}" style="display: none;">
-        <label for="q13${nextLetter}${index}">Specifics:</label>
-        <input type="text" id="q13${nextLetter}${index}" name="q13${nextLetter}${index}">
+      <div class="adjust-size" id="${containerId}" style="display: none;">
+        <label for="${containerId}">Specifics:</label>
+        <input type="text" id="${containerId}" name="${containerId}">
       </div>
     `;
   }
+
   const checkboxAndInputPairs = [
-    {
-      index: 1,
-      labelText: 'My home was air-leak tested and now it is leak-proof',
-      checkboxValue: 'air-leak',
-    },
-    {
-      index: 2,
-      labelText: 'I replaced my windows with energy efficient windows (e.g. double or triple-layer)',
-      checkboxValue: 'energy-efficient windows',
-    },
-    {
-      index: 3,
-      labelText: 'I installed a high efficiency furnace',
-      checkboxValue: 'high efficiency furnace',
-    },
-    {
-      index: 4,
-      labelText: 'I increased the thermal insulation of my building envelop (e.g. increase R-value, foam spray under roof, etc.)',
-      checkboxValue: 'thermal insulation',
-    },
-    {
-      index: 5,
-      labelText: 'installed a photovoltaic system',
-      checkboxValue: 'photovoltaic system',
-    },
-    {
-      index: 6,
-      labelText: 'I installed a heat pump for home heating',
-      checkboxValue: 'heat pump',
-    },
-    {
-      index: 7,
-      labelText: 'I installed a solar thermal collector',
-      checkboxValue: 'solar thermal collector',
-    },
-    {
-      index: 8,
-      labelText: 'I installed a thermal energy storage system',
-      checkboxValue: 'thermal energy storage system',
-    },
-    {
-      index: 9,
-      labelText: 'I have installed a green roof',
-      checkboxValue: 'green roof',
-    },
-    {
-      index: 10,
-      labelText: 'I have installed other systems not described above',
-      checkboxValue: 'other systems',
-    }
+    { checkboxId: "q13a1", containerId: "q13a2", labelText: 'My home was air-leak tested and now it is leak-proof', checkboxValue: 'air-leak' },
+    { checkboxId: "q13b1", containerId: "q13b2", labelText: 'I replaced my windows with energy efficient windows (e.g. double or triple-layer)', checkboxValue: 'energy-efficient windows' },
+    { checkboxId: "q13c1", containerId: "q13c2", labelText: 'I installed a high efficiency furnace', checkboxValue: 'high efficiency furnace' },
+    { checkboxId: "q13d1", containerId: "q13d2", labelText: 'I increased the thermal insulation of my building envelop (e.g. increase R-value, foam spray under roof, etc.)', checkboxValue: 'thermal insulation' },
+    { checkboxId: "q13e1", containerId: "q13e2", labelText: 'installed a photovoltaic system', checkboxValue: 'photovoltaic system' },
+    { checkboxId: "q13f1", containerId: "q13f2", labelText: 'I installed a heat pump for home heating', checkboxValue: 'heat pump' },
+    { checkboxId: "q13g1", containerId: "q13g2", labelText: 'I installed a solar thermal collector', checkboxValue: 'solar thermal collector' },
+    { checkboxId: "q13h1", containerId: "q13h2", labelText: 'I installed a thermal energy storage system', checkboxValue: 'thermal energy storage system' },
+    { checkboxId: "q13i1", containerId: "q13i2", labelText: 'I have installed a green roof', checkboxValue: 'green roof' },
+    { checkboxId: "q13j1", containerId: "q13j2", labelText: 'I have installed other systems not described above', checkboxValue: 'other systems' }
   ];
 
   const container = document.getElementById('checkbox-input-container');
   container.innerHTML = checkboxAndInputPairs
-    .map(({ index, labelText, checkboxValue }) => createCheckboxAndInputPair(index, labelText, checkboxValue))
+    .map(({ checkboxId, containerId, labelText, checkboxValue }) => createCheckboxAndInputPair(checkboxId, containerId, labelText, checkboxValue))
     .join('');
 
   function toggleTextInput(checkbox, container) {
@@ -91,42 +49,26 @@ document.addEventListener("DOMContentLoaded", function () {
       toggleTextInput(checkbox, container);
     });
   }
-
-
-  const checkboxes = [
-    { checkboxId: "q13a1", containerId: "q13b1" },
-    { checkboxId: "q13b2", containerId: "q13c2" },
-    { checkboxId: "q13c3", containerId: "q13d3" },
-    { checkboxId: "q13d4", containerId: "q13e4" },
-    { checkboxId: "q13e5", containerId: "q13f5" },
-    { checkboxId: "q13f6", containerId: "q13g6" },
-    { checkboxId: "q13g7", containerId: "q13h7" },
-    { checkboxId: "q13h8", containerId: "q13i8" },
-    { checkboxId: "q13i9", containerId: "q13j9" },
-    { checkboxId: "q13j10", containerId: "q13k10" },
-  ];
   
-  // const checkboxes = [
-  //   { checkboxId: "q13a1", containerId: "q13a2" },
-  //   { checkboxId: "q13b1", containerId: "q13b2" },
-  //   { checkboxId: "q13c1", containerId: "q13c2" },
-  //   { checkboxId: "q13d1", containerId: "q13d2" },
-  //   { checkboxId: "q13e1", containerId: "q13e2" },
-  //   { checkboxId: "q13f1", containerId: "q13f2" },
-  //   { checkboxId: "q13g1", containerId: "q13g2" },
-  //   { checkboxId: "q13h1", containerId: "q13h2" },
-  //   { checkboxId: "q13i1", containerId: "q13i2" },
-  //   { checkboxId: "q13j1", containerId: "q13j2" },
-  // ];
+  const checkboxes = [
+
+    { checkboxId: "q13a1", containerId: "q13a2" },
+    { checkboxId: "q13b1", containerId: "q13b2" },
+    { checkboxId: "q13c1", containerId: "q13c2" },
+    { checkboxId: "q13d1", containerId: "q13d2" },
+    { checkboxId: "q13e1", containerId: "q13e2" },
+    { checkboxId: "q13f1", containerId: "q13f2" },
+    { checkboxId: "q13g1", containerId: "q13g2" },
+    { checkboxId: "q13h1", containerId: "q13h2" },
+    { checkboxId: "q13i1", containerId: "q13i2" },
+    { checkboxId: "q13j1", containerId: "q13j2" },
+  ];
 
   // Wrap listener registration in a function
       checkboxes.forEach(({ checkboxId, containerId }) => {
       addCheckboxListener(checkboxId, containerId);
     });
-  
-    console.log(checkboxes);
-
-
+  });
 
 
 // prints the user inputs to a json file and allows the user to download it 
@@ -149,6 +91,12 @@ function printUserInputs() {
   var q9other = document.getElementById('q9other-input').value;
   // var q11 = document.getElementById('q11').value;
   // var q12 = document.getElementById('q12').value;
+
+  var q13a1 = document.getElementById('q13a1').value;
+  var q13a2 = document.getElementById('q13a2').value;
+  var q13b1 = document.getElementById('q13b1').value;
+  var q13b2 = document.getElementById('q13b2').value;
+
   var q16 = document.getElementById('q16').value;
   
   var q14a1 = document.getElementById('q14a1').value;
@@ -286,6 +234,10 @@ function printUserInputs() {
     q9: q9,
 
    
+    q13a1: q13a1,
+    q13a2: q13a2,
+    q13b1: q13b1,
+    q13b2: q13b2,
 
     q14a: q14a,
 
@@ -314,7 +266,6 @@ function printUserInputs() {
     document.body.removeChild(downloadLink);
 }
 
-});
 // the code below is responsible for hiding and displaying the "other" input field when users selet "other" from the dropdown menu
 //it automates this: 
                                                                                                                                                   // var q2 = document.getElementById("q2");
